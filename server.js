627fe726142321e4dc657b5237b30a06367d7367
Martinwarnman <martin.warnman@gmail.com>
2019-08-28 14:47:45 +0300

    added

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get("/getusers", (req, res, next) => {
       })
     });
 });
+
 app.get("/pns", (req, res, next) => {
     var sql = "SELECT users.player, games.name, ratings.stars from games inner join ratings on games.id = ratings.games_id inner join users on users.id = ratings.users_id"
     var params = []
@@ -44,55 +45,23 @@ app.get("/pns", (req, res, next) => {
       });
 });
 
-/*
-app.get("/api/cars/model/", (req, res, next) => {
-    var sql = "select model from cars"
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":rows
-        })
-      });
+
+
+
+app.post("/add", (req, res, next) => {
+ console.log(req.body.namn)
+  var sql = "INSERT INTO games(name) VALUES (?)"
+ db.run(sql, params, (err, row) => {
+ if (err) {
+ res.status(400).json({"error":err.message});
+ return;
+
+ res.json({
+ "message":"success"
+})
+}});
 });
-*/
-/*
-app.get("/api/cars/make/:make", (req, res, next) => {
-    var sql = "select * from cars where make = ?"
-    var params = [req.params.make]
-    db.all(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-*/
-/*
-app.get("/api/user/:id", (req, res, next) => {
-    var sql = "select * from user where id = ?"
-    var params = [req.params.id]
-    db.get(sql, params, (err, row) => {
-        if (err) {
-          res.status(400).json({"error":err.message});
-          return;
-        }
-        res.json({
-            "message":"success",
-            "data":row
-        })
-      });
-});
-*/
-// Root path
+
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
